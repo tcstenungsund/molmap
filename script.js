e2be6d34099ecc45e26110b298486ca1d1250517
dizzydrwaf <dizzydrwaf@gmail.com>
2025-11-17 13:46:23 +0100

    husky implumented and linting

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 function docReady(fn) {
   // see if DOM is already available
-  if (document.readyState === "complete" || document.readyState === "interactive") {
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
     // call on next available tick
     setTimeout(fn, 1);
   } else {
+    // biome-ignore lint/security/noSecrets: false positive, DOM event name
     document.addEventListener("DOMContentLoaded", fn);
   }
 }
 
-docReady(function () {
-  var resultContainer = document.getElementById('qr-reader-results');
-  var lastResult, countResults = 0;
+docReady(() => {
+  const result_container = document.getElementById("qr-reader-results");
+  let last_result,
+    count_results = 0;
 
-  var html5QrcodeScanner = new Html5QrcodeScanner(
-    "qr-reader", { fps: 10, qrbox: 250 });
+  const html5_qrcode_scanner = new Html5QrcodeScanner("qr-reader", {
+    fps: 10,
+    qrbox: 250,
+  });
 
-  function onScanSuccess(decodedText, decodedResult) {
-    if (decodedText !== lastResult) {
-      ++countResults;
-      lastResult = decodedText;
-      console.log(`Scan result = ${decodedText}`, decodedResult);
+  function onScanSuccess(decoded_text, _decoded_result) {
+    if (decoded_text !== last_result) {
+      ++count_results;
+      last_result = decoded_text;
+      // console.log(`Scan result = ${decoded_text}`, decoded_result);
 
-      resultContainer.innerHTML += `<div>[${countResults}] - ${decodedText}</div>`;
+      result_container.innerHTML += `<div>[${count_results}] - ${decoded_text}</div>`;
 
       // Optional: To close the QR code scannign after the result is found
-      html5QrcodeScanner.clear();
+      html5_qrcode_scanner.clear();
     }
   }
 
   // Optional callback for error, can be ignored.
-  function onScanError(qrCodeError) {
+  function onScanError(_qr_code_error) {
     // This callback would be called in case of qr code scan error or setup error.
     // You can avoid this callback completely, as it can be very verbose in nature.
   }
 
-  html5QrcodeScanner.render(onScanSuccess, onScanError);
+  html5_qrcode_scanner.render(onScanSuccess, onScanError);
 });
