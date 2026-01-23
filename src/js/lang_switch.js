c0fd31004b95aba49d4968748cea2b47ddc684b0
dizzydrwaf <dizzydrwaf@pm.me>
2026-01-23 09:34:27 +0100

    made the js button for switching lang

--- src/js/lang_switch.js
+++ src/js/lang_switch.js
@@ -1,18 +1,31 @@
 const switch_button = document.getElementById("switch_button");
+let lang_mode = "se";
+
+async function switchLang() {
+  const result = await getData();
+  document.getElementById("title-text").innerText =
+    result.lang[lang_mode].title;
+  document.getElementById("qr-code-instruction").innerText =
+    result.lang[lang_mode].qrCode;
+  lang_mode = lang_mode === "se" ? "en" : "se";
+}
 
 async function getData() {
-  const url = "./lang.json";
   try {
-    const response = await fetch(url);
+    const response = await fetch("./src/js/lang.json");
+
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const _result = await response.json();
-    /* console.log(result.text(), "test"); */
+    const result = await response.json();
+    /* console.log(result.lang.se.title, "test"); */
+    return result;
   } catch (_error) {
     /* console.error(error.message); */
   }
 }
 
-switch_button.addEventListener("click", getData);
+if (switch_button) {
+  switch_button.addEventListener("click", switchLang);
+}
